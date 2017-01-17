98226953aab267cb128cf84ca5e215b1c08948b1
Faisal Hassan <faisal.hassanx@gmail.com>
2017-01-17 22:12:03 +0000

    Add validations to project model for uniqueness and project limitations

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,12 @@
 class Project < ActiveRecord::Base
   belongs_to :tenant
+  validates_uniqueness_of :title
+  validate :free_plan_can_only_have_one_project
+  
+  def free_plan_can_only_have_one_project
+    if self.new_record? && (tenant.project.count > 0) && (tenant.plan == 'free')
+      errors.add(:base, "Free plans cannot have more than one project")
+    end
+  end
+  
 end
